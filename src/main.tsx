4be11b1368a392fa314bf904f4104f9e8f5ed0d1
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-27 08:27:53 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import CadastroProduto from './componentes/CadastroProduto';
+import CadastroProduto from './componentes/cadastroproduto/CadastroProduto';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto';
+import Header from './componentes/header/Header.tsx';
 
 const router = createBrowserRouter([
     {
-        path:"/",
-        element: <App />,
-    },
-    {
-    path: "/cadastro-produto",
-    element: <CadastroProduto/>
-    },
+        path: "/",
+        element: <><Header/><App /></>,
+      },
+      {
+        path: "/cadastro-produto",
+        element: <><Header/><CadastroProduto/></>,
+      },
+      {
+        path: "/alterar-produto/:id",
+        element: <><Header/><AlterarProduto/></>,
+      },
 ]);
 
 createRoot(document.getElementById('root')!).render(
