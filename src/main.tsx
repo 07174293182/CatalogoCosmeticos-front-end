a6e7028e5fbf37550ca188c76bfc3fb28745af93
Maria Rodrigues <118356268+07174293182@users.noreply.github.com>
2025-01-28 07:43:13 -0400

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-<<<<<<< HEAD
 import CadastroProduto from './componentes/cadastroproduto/CadastroProduto';
 import AlterarProduto from './componentes/alterarproduto/AlterarProduto';
 import Header from './componentes/header/Header.tsx';
-=======
-import CadastroProduto from './componentes/CadastroProduto.tsx';
->>>>>>> 26cd48bbd1edc4b9bb0fab19dc84b638f9fe405e
 
 const router = createBrowserRouter([
     {
